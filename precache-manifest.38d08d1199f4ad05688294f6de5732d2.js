9582ee157b7f8ff48d068787567a123ed5138651
Tomasz Puscian <pusciant@gmail.com>
2020-06-20 17:21:04 +0200

    Updates

---
+++ precache-manifest.38d08d1199f4ad05688294f6de5732d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c928e2c192da4b017ed8d0538b7282",
+    "url": "/Cloudify/index.html"
+  },
+  {
+    "revision": "764e3c831b750e723c5b",
+    "url": "/Cloudify/static/css/main.44cb3e86.chunk.css"
+  },
+  {
+    "revision": "56508f4ebe07e67d8417",
+    "url": "/Cloudify/static/js/2.3f322e6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cloudify/static/js/2.3f322e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764e3c831b750e723c5b",
+    "url": "/Cloudify/static/js/main.23d8e100.chunk.js"
+  },
+  {
+    "revision": "7078ca8a0caf176e19cb",
+    "url": "/Cloudify/static/js/runtime-main.6b92c5a1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Cloudify/precache-manifest.38d08d1199f4ad05688294f6de5732d2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cloudify/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
